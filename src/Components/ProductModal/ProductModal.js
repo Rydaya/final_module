e44861d330d7cc431da60bdfa7b2f296cc4b7baf
Kseniia <rydaya97@gmail.com>
2022-09-24 18:52:24 +0300

    add product modal

---
+++ src/Components/ProductModal/ProductModal.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './productModal.scss'
+
+const ProductModal = ({isActive, setIsActive, array}) =>{
+    return(
+        <div className={isActive ? 'modal modal__active' : 'modal'} onClick={() => setIsActive(false)}>
+            {array ? array.map(({ id, category, imgUrl, name, price, weight, pieces, ingredients }) => (
+                <div className="modal__content" key={id} onClick={e => e.stopPropagation()}>
+                    <div className="modal__img">
+                        <img src={imgUrl} alt={name} width={250}/>
+                    </div>
+                    <div className="modal__info">
+                        <div className="modal__title">{name}</div>
+                        <div className="modal__about">{weight} {category === "drinks" ? "мл" : "грамм"}, {pieces} шт</div>
+                        <div className="buyblock modal__buyblock">
+                            <div className="price modal__price">{price} ₴</div>
+                            <button className="btn modal__btn" onClick={e => e.stopPropagation()}>Хочу!</button>
+                        </div>
+                        <div className="modal__desc">Состав: {ingredients}</div>
+                    </div>
+                </div>
+            )) : <></>}
+        </div>
+    )
+}
+
+export default ProductModal;
--- src/Components/Wrapper/Wrapper.js
+++ src/Components/Wrapper/Wrapper.js
@@ -1,22 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import Content from "../Content/Content";
 import OrderSection from "../OrderSection/OrderSection";
 import Footer from "../Footer/Footer";
-import './wrapper.scss';
+import ProductModal from "../ProductModal/ProductModal";
+import data from '../../data.js';
+import "./wrapper.scss";
 
 const Wrapper = () => {
+  const [currentData, setCurrentData] = useState(null);
+  const [productModalActive, setProductModalActive] = useState(false);
+  const [clickedProduct, setClickedProduct] = useState(null);
 
-    return (
-        <div className="wrapper">
-            <Header/>
-            <Sidebar/>
-            <Content />
-            <OrderSection/>
-            <Footer/>
+  useEffect(() => {
+    setCurrentData(data);
+  }, []);
+
+  const getClickedProduct = (clickedId) => {
+    setClickedProduct([currentData.find(item => item.id === clickedId)]);
+  }
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <Sidebar />
+      <Content>
+        <div className="products">
+          {currentData ? currentData.map(({ id, category, imgUrl, name, price, weight, pieces }) => (
+            <div className="card" key={id} onClick={() => {setProductModalActive(true); getClickedProduct(id)}}>
+              <div className="card__info">
+                <div className="card__img">
+                  <img src={imgUrl} alt={name} width={190} />
+                </div>
+                <div className="card__title">{name}</div>
+                <div className="card__about">
+                  {weight} {category === "drinks" ? "мл" : "грамм"}, {pieces} шт
+                </div>
+              </div>
+              <div className="buyblock card__buyblock">
+                <div className="price card__price">{price} ₴</div>
+                <button className="btn card__btn" onClick={e => e.stopPropagation()}>Хочу!</button>
+              </div>
+            </div>
+          )) : <div className="products_empty">Что-то пошло не так...</div>}
         </div>
-    )
-}
+        <ProductModal isActive={productModalActive} setIsActive={setProductModalActive} array={clickedProduct}/>
+      </Content>
+      <OrderSection />
+      <Footer />
+    </div>
+  );
+};
 
 export default Wrapper;
+
+
